7a66db2632c52552d596557429fc9a51bc78dc16
Mathieu Suffert <mathieu.suffert@gmail.com>
2017-04-10 22:25:53 +0200

    sliding en cours de travail

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 $(document).foundation();
+
+var carousel;
+
+$(document).ready(function () {
+    carousel = $(".test");
+    carousel.itemslide(
+      {
+        disable_scroll:true,
+        start:0,
+        parent_width :true
+      }
+    );
+});
